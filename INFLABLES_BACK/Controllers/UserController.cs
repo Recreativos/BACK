d48d619d4dd2a2f62469f620b2ed35276e9ccfde
2harold2 <145359297+2harold2@users.noreply.github.com>
2024-03-19 15:26:30 -0500

    .

---
+++ INFLABLES_BACK/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace INFLABLES_BACK.Controllers;
+
+[Route("api/user")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    #region Recuperar de contraseña
+    // POST: registerUser
+    /// <summary>
+    /// Crear un usuario
+    /// </summary>
+    /// <remarks>
+    /// Método que permite registrar un nuevo usuario
+    /// </remarks>
+    /// <response code="401">Unauthorized. No se ha indicado o es incorrecto el token JWT de acceso</response>
+    [HttpPost]
+    [Route("registerUser")]
+    public async Task<IActionResult> RecoverPasswordByEmail([FromBody] dynamic request)
+    {
+        return Ok();
+    }
+    #endregion
+}
